30dff37b08dc260155c886730e5ed516ea005f5d
Arjun Sah <arjunsah@sussol.net>
2023-12-07 17:49:18 +0545

    Introduced mongo db

---
+++ mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('At least give password as argument');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://arjunsah:${password}@cluster0.f9ecvpj.mongodb.net/phonebook?retryWrites=true&w=majority`;
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+if (process.argv.length > 3) {
+  const person = new Person({ name, number });
+  person.save().then(() => {
+    console.log(`Added ${name} number ${number} to phonebook.`);
+    mongoose.connection.close();
+  });
+} else {
+  Person.find({}).then((persons) => {
+    console.log('Phonebook:');
+    persons.map((person) => console.log(`${person.name} ${person.number}`));
+    mongoose.connection.close();
+  });
+}
